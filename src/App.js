c5fd4ceb1ac8146ea5a4077c093cf465a07f0041
Kumar Dulal <nitinkc75@gmail.com>
2024-09-15 05:38:51 +0545

    chnages small margin homecontainer

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        
           <Route path="/" element={<Homepage/>} />
           <Route path="/home" element={<Homepage/>} />
           <Route path="/Liquor" element={<Liqureshop/>} />
