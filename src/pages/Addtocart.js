d1aee2d264143cd0fcdcb54bff7503c2ae894181
Kumar Dulal <nitinkc75@gmail.com>
2025-06-11 02:19:10 +0545

    itemcount to1 minimum

--- src/pages/Addtocart.js
+++ src/pages/Addtocart.js
@@ -61,7 +61,7 @@ const AddToCart = () => {
   const decrement = (id) => {
     setCartItems((prevItems) =>
       prevItems.map((item) =>
-        item.cartId === id && item.quantity > 0
+        item.cartId === id && item.quantity > 1
           ? { ...item, quantity: item.quantity - 1 }
           : item
       )
