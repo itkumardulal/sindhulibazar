f1ad066a3c30111ab9d35318eb7a3d0d3547b58e
Kumar Dulal <nitinkc75@gmail.com>
2025-06-26 15:49:04 +0545

    logod

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -141,6 +141,7 @@ function DrawerAppBar({ window, children }) {
           // #FBA518
           // #E52020
           // #F9CB45
+          // #04ccdaf6
           background: "linear-gradient(135deg, #F9CB45, #E52020)",
         }}
       >
